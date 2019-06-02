2de38302a838f81bf07abaa104679d31f4bca13a
Javier Soria <javier.soria@gmail.com>
2019-06-01 22:37:41 -0700

    added psuedo code to index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+$(function() {
+   // 1. Set event listener for submit button
+   //    1a. Stop default form submission behavior
+   // 2. Get value from #shopping-list-entry
+   // 3. Append ul class .shopping-list with item
+   //    3a. Clear input value
+   // 4. Add event listener on check button with class .shopping-item-toggle on click
+   //    4a. Toggle class .shopping-item__checked to span class .shopping-item
+   //    4b. If check is clicked again, toggle off .shopping class on text
+   // 5. Add event listener on delete button with class .shopping-item-delete
+   // 6. Remove li item
+}
