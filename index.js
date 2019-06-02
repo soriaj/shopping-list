ab98b706d1075de55d017328a64a100d2502dbd2
Javier Soria <javier.soria@gmail.com>
2019-06-01 23:53:09 -0700

    added functionality for Add item button. Testing Check button functionality

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 $(function() {
    // 1. Set event listener for submit button
-   //    1a. Stop default form submission behavior
-   // 2. Get value from #shopping-list-entry
-   // 3. Append ul class .shopping-list with item
-   //    3a. Clear input value
-   // 4. Add event listener on check button with class .shopping-item-toggle on click
-   //    4a. Toggle class .shopping-item__checked to span class .shopping-item
+   $('#js-shopping-list-form').submit(function(event) {
+      // 1a. Stop default form submission behavior
+      event.preventDefault();
+      // 2. Get value from #shopping-list-entry
+      let item = $(this).find('#shopping-list-entry');
+      
+      // 3. Append ul class .shopping-list with item
+      $('.shopping-list').append(`
+         <li>
+         <span class="shopping-item">${item.val()}</span>
+         <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+               <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+               <span class="button-label">delete</span>
+            </button>
+         </div>
+         </li>
+      `);
+      // 3a. Clear input value
+      item.val("");
+   });
+   
+   // 4. Add event listener on existing element ul, specifically with class .shopping-item-toggle on button
+   // with event deletegation
+   $('ul').on('click', '.shopping-item-toggle', function(event){
+      // 4a. Get current target
+      const targetItem = $(this).parent().parent();
+      const otherItems = $('.shopping-item').not(targetItem);
+      otherItems.removeClass('shopping-item__checked');
+      targetItem.toggleClass('shopping-item__checked');
+   })
+
    //    4b. If check is clicked again, toggle off .shopping class on text
    // 5. Add event listener on delete button with class .shopping-item-delete
    // 6. Remove li item
-}
+})
